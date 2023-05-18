f1e90a5940e076774ac990ccd6d058a954e46d7e
Vikas Mathpati <mathpativikas198@gmail.com>
2023-05-18 17:34:23 +0530

    Variables

--- bin/main.dart
+++ bin/main.dart
@@ -3,4 +3,16 @@ void main() {
 
 //Variable Name and Rules
 //Variable is an identifier
+// Valid varible name
+  var name = "1234";
+  var f_name = "dsa";
+
+  // no space in varibale name
+  var soni12 = "sas";
+  var $dadu = "dsa";
+
+  // Invalid variable name
+  //cant start variable name with numbers and reserved keywords
+// Printing variable
+  print(name);
 }
